efcec446fc789f5a27965070d90ff304e3b5b646
mykha <efimenko.mykhailo@gmail.com>
2020-04-29 16:12:26 +0300

    Add recodring to file *txt + .gitignore

--- barbershop.rb
+++ barbershop.rb
@@ -11,6 +11,10 @@ post '/' do
 	@date_time = params[:date_time]
 	@title = "Thank you !"
 	@message = "Dear #{@user_name}, we are waiting for you #{@date_time}"
+
+	input = File.open "./clients.txt", "a"
+	input.puts "client=#{@user_name};phone=#{@phone};date_time=#{@date_time}"	
+	input.close
 	erb :message
 
 end
