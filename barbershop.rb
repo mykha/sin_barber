897696f27d969a139a6687246700fa36052d07a8
mykha <efimenko.mykhailo@gmail.com>
2020-05-06 21:33:41 +0300

    Add Bootstrap layout and reloader

--- barbershop.rb
+++ barbershop.rb
@@ -1,4 +1,15 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+configure do
+  enable :sessions
+end
+
+helpers do
+  def username
+    session[:identity] ? session[:identity] : 'Hello stranger'
+  end
+end
 
 get '/' do
 	erb :index
