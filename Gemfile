84f8c34d884fc9b73318d80f5fa75ca96789017a
mykha <efimenko.mykhailo@gmail.com>
2021-06-06 19:40:08 +0300

    Booking form added

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
-gem 'sinatra'
+gem "sinatra", "~> 1.4.8"
 gem 'sinatra-reloader'
 gem 'bootstrap', '~> 4.3.1'
+gem "data_mapper"
+gem 'dm-sqlite-adapter'
--- barbershop.rb
+++ barbershop.rb
@@ -1,8 +1,64 @@
 require 'sinatra'
 #require 'sinatra/reloader'
+require './secure.rb'
+
+
+=begin
+class User
+	include DataMapper::Resource
+	property :id, Serial
+	property :username, String, :required => true, key: true, unique_index: true
+	property :userpass, String, :required => true
+	property :supervisor, Boolean, :default => false
+#	property :password, String, length: 10..255
+end
+
+class Barber
+	include DataMapper::Resource
+	property :id, Serial
+	property :barbername, String, :required => true, key: true, unique_index: true
+	property :username, String, :required => true, key: true, unique_index: true
+#	property :password, String, length: 10..255
+end
+
+
+class Order
+	include DataMapper::Resource
+	property :id, Serial
+	property :client_name, Text, :required => true
+	property :client_phone, Text, :required => true
+	property :order_date,  DateTime, :required => true
+	has n, :services
+	belongs_to :barber
+end
+
+class Service
+	include DataMapper::Resource
+	property :id, Serial
+	property :name, Text, :required => true
+	has n, :tasks
+end
+
+DataMapper.finalize.auto_upgrade!
+>>>>>>> bcd6be1158d4cdfb0a7cd878489682003ea44cf6
+=end
 
 configure do
   enable :sessions
+end
+
+helpers do
+  def username
+    session[:identity] ? session[:identity] : 'Hello stranger'
+  end
+end
+
+before '/secure/*' do
+  unless session[:identity]
+    session[:previous_url] = request.path
+    @error = 'Sorry, you need to be logged in to visit ' + request.path
+    halt erb(:login_form)
+  end
 end
 
 helpers do
@@ -21,20 +77,27 @@ get '/signupforashave' do
 end
 
 get '/about' do
-	erb "<div class=\"jumbotron text-center\"> <p>bear can not bear beards, but our bears can bear</p></div><h1></h1>"
+	erb "<div class=\"jumbotron text-center\"> <p>a bear can not bear beards, but our bears can bear</p></div><h1></h1>"
+	#erb :forshave
+end
+
+get '/signupforashave' do
+	erb "<h1>sign up for a shave</h1>"
 	#erb :forshave
 end
 
 post '/' do
 
-	@user_name = params[:user_name]
-	@phone = params[:phone]
-	@date_time = params[:date_time]
+	@user_name = params[:inputName]
+	@phone = params[:inputPhone]
+	@date_time = params[:inputDate]
+	@service = params[:inputService]
+	@barber = params[:inputBarber]
 	@title = "Thank you !"
 	@message = "Dear #{@user_name}, we are waiting for you #{@date_time}"
 
 	input = File.open "./clients.txt", "a"
-	input.puts "client=#{@user_name};phone=#{@phone};date_time=#{@date_time}"	
+	input.puts "client=#{@user_name};phone=#{@phone};date_time=#{@date_time}; service=#{@service};barber=#{@barber};"	
 	input.close
 	erb :message
 
